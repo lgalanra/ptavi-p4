07bc7867bc9a292cd3c054b9fc36ed965347d669
Luis Galan Ramil <lgalanra@alpha20.aulas.gsyc.urjc.es>
2016-10-24 21:09:07 +0200

    hecho pep8

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import sys
 
 
 if __name__ == "__main__":
-# Constantes. Dirección IP del servidor y contenido a enviar
+    # Constantes. Dirección IP del servidor y contenido a enviar
     try:
         SERVER = sys.argv[1]
         PORT = int(sys.argv[2])
@@ -17,16 +17,17 @@ if __name__ == "__main__":
         direction = sys.argv[4]
         expires_value = int(sys.argv[5])
     except ValueError:
-        sys.exit("Usage: client.py ip puerto register sip_address expires_value")
+        sys.exit("Usage: client.py ip puerto register\
+                 /sip_address expires_value")
 
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((SERVER, PORT))
-""" Creación del mensaje completo """
         if sys.argv[3] == 'register':
-            my_socket.send(bytes('REGISTER sip:' + sys.argv[4] +' SIP/2.0','utf-8')
-                            + b'\r\n' + bytes('Expires: '+ str(expires_value),'utf-8')
-                             + b'\r\n\r\n')
+            my_socket.send(bytes('REGISTER sip:' + sys.argv[4] +
+                           ' SIP/2.0', 'utf-8') + b'\r\n' +
+                           bytes('Expires: ' + str(expires_value), 'utf-8') +
+                           b'\r\n\r\n')
 
         data = my_socket.recv(1024)
         print(data.decode('utf-8'))
