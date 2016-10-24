2219662f95bcdfbd7a93ed105dd58af34b7e9684
Luis Galan Ramil <lgalanra@gamma35.aulas.gsyc.urjc.es>
2016-10-24 18:12:16 +0200

    hecho ejercicio 6

--- client.py
+++ client.py
@@ -7,23 +7,26 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
+
+if __name__ == "__main__":
 # Constantes. Dirección IP del servidor y contenido a enviar
-try:
-    SERVER = sys.argv[1]
-    PORT = int(sys.argv[2])
-    lista = []
-    lista = sys.argv[3:]
-    texto = " ".join(lista)
-    LINE = texto
-except ValueError:
-    sys.exit("Llamada incorrecta al programa")
+    try:
+        SERVER = sys.argv[1]
+        PORT = int(sys.argv[2])
+        register = sys.argv[3]
+        direction = sys.argv[4]
+    except ValueError:
+        sys.exit("Llamada incorrecta al programa")
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, PORT))
 
-print("Socket terminado.")
+        if sys.argv[3] == 'register':
+            my_socket.send(bytes('REGISTER sip:' + sys.argv[4] +' SIP/2.0','utf-8')
+                            + b'\r\n\r\n')
+
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
+
+        print("Socket terminado.")
