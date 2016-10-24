a4ebfd2bb6431a5a070b437d73237ae257ef2b62
Luis Galan Ramil <lgalanra@gamma35.aulas.gsyc.urjc.es>
2016-10-24 18:12:06 +0200

    hecho ejercicio 6

--- server.py
+++ server.py
@@ -7,20 +7,26 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
-
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     SIP Register server class
     """
     dicc = {}
-    skdjhdjhfd
 
     def handle(self):
-        self.wfile.write(b"Hemos recibido tu peticion")
         print(self.client_address[0])
         print(self.client_address[1])
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+
+        request = self.rfile.read().decode('utf-8')
+        print(request)
+        fields = request.split(' ')
+
+        if fields[0] == 'REGISTER':
+            login = fields[1].split(':')
+            self.dicc[login[1]] = self.client_address[0]
+            self.wfile.write(b"SIP/2.0 200 OK " + b'\r\n\r\n')
+            print(self.dicc)
+
 
 if __name__ == "__main__":
     try:
